1dec1f242e73702a146e3f050be63b15438ed2aa
algoccs <algodemoacc@gmail.com>
2026-04-17 16:31:18 -0400

    Create config.py

---
+++ shooter/config.py
@@ -0,0 +1,15 @@
+# CONSTANTES
+
+ANCHO, ALTO = 640, 480
+TITULO = 'Proyecto...'
+COLOR_FONDO = (220, 120, 30)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+FPS = 60
+PLAYER_IMG = 'link.png'
+ENEMY_IMG = 'octo.png'
+BULLET_IMG = 'link.png'
+BG_IMG = 'bg.gif'
+WIN_IMG = ''
+GAMEOVER_IMG = ''
+FONT_FILE = 'Sekuya-Regular.ttf'
